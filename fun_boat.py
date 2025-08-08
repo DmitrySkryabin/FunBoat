e53f244006d0d525ee058aa81f6b2096f7bb8e65
Dmitry Skryabin <skryabin_ds@albank.ru>
2025-08-08 14:17:02 +0900

    add funning

--- fun_boat.py
+++ fun_boat.py
@@ -1,19 +1,25 @@
+import logging
+
 class FunBoat:
 
     BOAT_CAPACITY = 6 # Вместимость лодки
     DIRECTION = ['n', 'w', 's', 'e']
+    
+    LOG = logging.getLogger(__name__)
     
     def __init__(self):
         self.rowers = 0 # Гребцы
         self.is_moving = False 
         self.direction = 'n' # Начальное направление на север
         self.speed = 0
+        self.song_power = 0 # Громкость песни
         
     def __udpate_speed(self):
         if self.is_moving:
-            self.speed = self.rowers * 0.8 # Взял с головы, типа 2 гребца не гребят в 2 раза больше 1
+            self.speed = self.rowers * 0.8 # Взял с головы, 2 гребца не гребут в 2 раза больше 1
         else:
             self.speed = 0
+        self.LOG.info(f'Speed updated: {self.speed}')
         
     def add_rowers(self, count_of_rowers):
         if self.rowers + count_of_rowers > self.BOAT_CAPACITY:
@@ -35,7 +41,6 @@ class FunBoat:
         self.is_moving = False
         self.__udpate_speed()
         
-        
     def turn(self, direction: str):
         '''
         Поворачиваем лодку в направлении стороны света
@@ -53,10 +58,30 @@ class FunBoat:
             raise RuntimeError('Cant turn when boat not moving')
         self.direction = direction
         
+    def start_funning(self):
+        '''
+        Делаем лодку веселой
+        '''
+        if self.rowers == 0:
+            raise RuntimeError('No one to sing songs ')
+        self.song_power = self.rowers * 50 # Примерные герцы
+        self.LOG.info(f'Yo, Ho haul together, hoist the colours high...')
+        
+    def stop_funning(self):
+        '''
+        Делаем лодку не веселой
+        '''
+        self.song_power = 0
+        self.LOG.info(f'Silence over the water surface')
+        
     def get_status(self):
         return {
             'rowers': self.rowers,
             'is_moving': self.is_moving,
             'direction': self.direction,
-            'speed': self.speed
+            'speed': self.speed,
+            'song_power': self.song_power
         }
+        
+
+logging.basicConfig(level=logging.INFO)
