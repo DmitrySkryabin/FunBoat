b8af9db4e47173af40d896f1c977e9fde6cc0e05
Dmitry Skryabin <skryabin_ds@albank.ru>
2025-08-08 15:50:13 +0900

    update eng

--- fun_boat.py
+++ fun_boat.py
@@ -2,55 +2,55 @@ import logging
 
 class FunBoat:
 
-    BOAT_CAPACITY = 6 # Вместимость лодки
+    BOAT_CAPACITY = 6  # Вместимость лодки
     DIRECTION = ['n', 'w', 's', 'e']
-    
+
     LOG = logging.getLogger(__name__)
-    
+
     def __init__(self):
-        self.rowers = 0 # Гребцы
+        self.rowers = 0  # Гребцы
         self.is_moving = False 
-        self.direction = 'n' # Начальное направление на север
+        self.direction = 'n'  # Начальное направление на север
         self.speed = 0
-        self.song_power = 0 # Громкость песни
-        
+        self.song_power = 0  # Громкость песни
+
     def __udpate_speed(self):
         if self.is_moving:
-            self.speed = self.rowers * 0.8 # Взял с головы, 2 гребца не гребут в 2 раза больше 1
+            self.speed = self.rowers * 0.8  # Взял с головы, 2 гребца не гребут в 2 раза больше 1
         else:
             self.speed = 0
         self.LOG.info(f'Speed updated: {self.speed}')
-        
+
     def __update_song_power(self):
-        self.song_power = self.rowers * 50 # Примерные герцы
+        self.song_power = self.rowers * 50  # Примерные герцы
         self.LOG.info(f'Song power updated: {self.song_power}')
-        
+
     def add_rowers(self, count_of_rowers):
         if self.rowers + count_of_rowers > self.BOAT_CAPACITY:
-            raise ValueError('To many rowers')
+            raise ValueError('Too many rowers. Boat capacity exceeded.')
         self.rowers += count_of_rowers
         self.__update_song_power()
-        
+
     def remove_rowers(self, count_of_rowers):
         if self.rowers - count_of_rowers < 0:
-            raise ValueError('Cant remove more rowers than you have')
+            raise ValueError('Cannot remove more rowers than are present.')
         self.rowers -= count_of_rowers
         self.__update_song_power()
-        
+
     def start_rowing(self):
         if self.rowers == 0:
-            raise RuntimeError('Cant row without rowers')   
+            raise RuntimeError('Cannot start rowing without rowers.')
         self.is_moving = True
         self.__udpate_speed()
-        
+
     def stop_rowing(self):
         self.is_moving = False
         self.__udpate_speed()
-        
+
     def turn(self, direction: str):
         '''
         Поворачиваем лодку в направлении стороны света
-        
+
         Args:
             direction (str): Сторона света:
                 - n: Север
@@ -59,27 +59,27 @@ class FunBoat:
                 - e: Восток
         '''
         if direction not in self.DIRECTION:
-            raise ValueError('Direction must be: "n", "w", "s", "e"')
+            raise ValueError('Direction must be one of: "n", "w", "s", "e".')
         if not self.is_moving:
-            raise RuntimeError('Cant turn when boat not moving')
+            raise RuntimeError('Cannot turn while the boat is not moving.')
         self.direction = direction
-        
+
     def start_funning(self):
         '''
         Делаем лодку веселой
         '''
         if self.rowers == 0:
-            raise RuntimeError('No one to sing songs ')
+            raise RuntimeError('Cannot start funning without rowers.')
         self.__update_song_power()
         self.LOG.info(f'Yo, Ho haul together, hoist the colours high...')
-        
+
     def stop_funning(self):
         '''
         Делаем лодку не веселой
         '''
         self.song_power = 0
         self.LOG.info(f'Silence over the water surface')
-        
+
     def get_status(self):
         return {
             'rowers': self.rowers,
@@ -88,6 +88,5 @@ class FunBoat:
             'speed': self.speed,
             'song_power': self.song_power
         }
-        
 
 logging.basicConfig(level=logging.INFO)
